581717b354689fab433670671723d629e2debd9c
timothy <florenceut.chan@mail.utoronto.ca>
2016-02-20 21:06:38 -0500

    switch dependent failsafe and boundary conditions added

--- Arduino_PWMRelay/Arduino_PWMRelay_ino/Arduino_PWMRelay_ino.ino
+++ Arduino_PWMRelay/Arduino_PWMRelay_ino/Arduino_PWMRelay_ino.ino
@@ -1,5 +1,5 @@
-double channel0[2]; //throttle receiver
-double channel1[2]; //rudder receiver
+double channel0; //throttle receiver
+double channel1; //rudder receiver
 double x0;
 double x1;
 int elevator = 3;
@@ -10,6 +10,7 @@ double wiie;
 double wiir;
 double wiia;
 double wiit;
+int switchd;
 
 void setup() {
   // put your setup code here, to run once:
@@ -32,29 +33,49 @@ double wiileftthrot,
 double wiileftrudd,
 double wiirightail) {
 
+/*
 for (int y= 0; y<2; y++){ 
    channel0[y] = pulseIn(5, HIGH); //throttle
 } 
 for (int i = 0; i <2; i++){
    channel1[i] = pulseIn(6, HIGH); //elevator
 }
-
-x0 = ((channel0[1]-1290)*255/1300)-1;
-x1 = ((channel1[1]-1290)*255/1300)-1;
-
-  Serial.println(channel0[0]);
-  Serial.println("  |  ");
-  Serial.println(channel1[0]); 
+*/
+channel0 = pulseIn(5, HIGH); //throttle
+channel1 = pulseIn(6, HIGH); //rudder
 
 
+x0 = ((channel0-1290)*255/1500)-1;
+x1 = ((channel1-1290)*255/1500)-1;
 
+  Serial.print(channel0);
+  Serial.print("  |  ");
+  Serial.print(x0);
+  Serial.print("  |  ");
+  Serial.print(channel1); 
+  Serial.print("  |  ");
+  Serial.println(x1);
+//read switch D from transmitter
+switchd = pulseIn (2, HIGH);
+
+/*
 //if some threshold for fail safe
 if (abs(channel0[0]-channel0[1])>100 || abs(channel1[0]-channel1[1])>100)
 {
    analogWrite (throttle, channel0[1]);
    analogWrite (rudder, channel1[1]);
 }  
+*/
 
+if (switchd < 1900)
+{
+  analogWrite (rudder, x1);
+  analogWrite (throttle, x0);
+  if (channel1>=2590){
+    analogWrite (rudder, 2589);}
+  if (channel1<=1310){
+    analogWrite (rudder, 1310);}
+}
 else
 {
  
@@ -68,10 +89,45 @@ else
   Serial.println(wiir);
   Serial.println(wiia);
   
-  analogWrite (elevator,wiie );
+  //////////elev//////////
+  if ( wiirightelev < 1300){
+    analogWrite (elevator, 1300);}
+  else 
+    { if (wiirightelev > 2580)
+      {analogWrite (elevator, 2575);}
+  else {analogWrite (elevator,wiie );}
+  }
+  /////////throt///////////////////
+  if ( wiileftthrot < 1300){
+    analogWrite (throttle, 1300);}
+  else 
+    { if (wiileftthrot > 2580)
+      {analogWrite (throttle, 2575);}
+  else {analogWrite (throttle,wiit );}
+  }
+  //////rudd/////////////////
+    if ( wiileftrudd < 1300){
+    analogWrite (rudder, 1300);}
+  else 
+    { if (wiileftrudd > 2580)
+      {analogWrite (rudder, 2575);}
+  else {analogWrite (rudder,wiir );}
+  }
+  //////ail/////////////
+    if ( wiirightail < 1300){
+    analogWrite (aileron, 1300);}
+  else 
+    { if (wiirightail > 2580)
+      {analogWrite (aileron, 2575);}
+  else {analogWrite (aileron,wiia );}
+  }
+  ////////////////////////
+  /*
+  analogWrite (elevator,wiie )
   analogWrite (throttle,wiit );  
   analogWrite (rudder, wiir); 
   analogWrite (aileron, wiia);
+*/
 }
   
 }
---
+++ Arduino_PWMRelay/Arduino_PWMRelay_ino/Arduino_PWMRelay_ino_ino/Arduino_PWMRelay_ino_ino.ino
@@ -0,0 +1,69 @@
+double channel0[2]; //throttle receiver
+double channel1[2]; //rudder receiver
+double x0;
+double x1;
+int elevator = 3;
+int aileron = 9;
+int throttle = 10;
+int rudder = 11;
+double wiie;
+double wiir;
+double wiia;
+double wiit;
+
+void setup() {
+  // put your setup code here, to run once:
+
+  pinMode (5, INPUT); //from receiver throttle
+  pinMode (6, INPUT); //from receiver elevator
+  pinMode (elevator, OUTPUT); //to HK elevator
+  pinMode (aileron, OUTPUT); //to HK aileron
+  pinMode (throttle, OUTPUT); //to HK throttle
+  pinMode (rudder, OUTPUT); //to HK rudder
+  Serial.begin (9600);
+}
+
+void loop () {
+  func (0.1, 0.5, 0.7, -0.5);
+}
+
+void func (double wiirightelev,
+double wiileftthrot,
+double wiileftrudd,
+double wiirightail) {
+
+for (int y= 0; y<2; y++){ 
+   channel0[y] = pulseIn(5, HIGH); //throttle
+} 
+for (int i = 0; i <2; i++){
+   channel1[i] = pulseIn(6, HIGH); //elevator
+}
+
+x0 = ((channel0[1]-1290)*255/1300)-1;
+x1 = ((channel1[1]-1290)*255/1300)-1;
+
+  Serial.println(channel0[0]);
+  Serial.println("  |  ");
+  Serial.println(channel1[0]); 
+
+
+//if some threshold for fail safe
+if (abs(channel0[0]-channel0[1])>100 || abs(channel1[0]-channel1[1])>100)
+{
+   analogWrite (throttle, channel0[1]);
+   analogWrite (rudder, channel1[1]);
+}  
+else
+{
+  wiie = ((wiirightelev-1290)*255/1300)-1; 
+  wiit = ((wiileftthrot-1290)*255/1300)-1; 
+  wiir = ((wiileftrudd-1290)*255/1300)-1; 
+  wiia = ((wiirightail-1290)*255/1300)-1;
+  
+  analogWrite (elevator,wiie );
+  analogWrite (throttle,wiit );  
+  analogWrite (rudder, wiir); 
+  analogWrite (aileron, wiia);
+}
+  
+}
